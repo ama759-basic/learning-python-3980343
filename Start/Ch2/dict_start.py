13b682529810c73a4f75b8673ca44199e02bb1c0
ama759-basic <ama759@gmail.com>
2026-01-22 19:05:42 +0000

    Module 4 complete

--- Start/Ch2/dict_start.py
+++ Start/Ch2/dict_start.py
@@ -3,21 +3,34 @@
 
 
 # Dictionary: a key-value data structure
+myDict={
+  "one":1,
+  "two":2,
+  3:"three",
+  4.5:["four",".",5]
+}
 
+print(myDict,len(myDict))
 
 # dictionaries are accessed via keys
-
+print(myDict["one"])
+print(myDict[4.5])
 
 # you can also set dictionary data by creating a new key
-
+myDict["five"]=False
+print(myDict,len(myDict))
 
 # Trying to access a nonexistent key will produce an error
 
 
 # To avoid this, you can use the "in" operator to see if a key exists
-
+print("five" in myDict)
+print(2 in myDict)
 
 # You can retrieve all of the keys and values from a dictionary
-
+print(myDict.keys())
+print(myDict.values())
 
 # You can also iterate over all the items in a dictionary
+for key,val in myDict.items():
+  print(key,val)
