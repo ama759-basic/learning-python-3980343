66e52b5dfb8c4d619da926c99db790b8a7d2a08c
ama759-basic <ama759@gmail.com>
2026-01-22 21:10:01 +0000

    Module 6 Complete

--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -5,15 +5,30 @@
 x = 0
 
 # define a while loop
+# while x < 5:
+#   print(x)
+#   x = x +1
 
+# answer = input("Stop?")
+# while answer != "Y":
+#   print(answer)
+#   answer = input("Stop?")
 
 # define a for loop
-
+# days = "MTWTFSS"
+# for d in days:
+#   print(d)
 
 # use a for loop over a collection
 
 
 # use the break and continue statements
+days = "MTWTFSS"
+for d in days:
+  # if (d == "W"): break
+  if (d == "T"): continue
+  print(d)
 
 
 # using the enumerate() function to get an index and an item
+for i,d in enumerate(days): print(i,d)
